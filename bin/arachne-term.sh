473dc055efcc924b82dffeaed815cec9c1fdf4c5
dlarv <hyrum21344@gmail.com>
2024-02-13 21:17:35 -0500

    Only one instance of arachne terminal can be opened at a time

--- bin/arachne-term.sh
+++ bin/arachne-term.sh
@@ -6,10 +6,16 @@
 # Outputs working directory to arachne-launcher 
 
 function _get_active_window() {
-	# This will be swaywm specific
-	focusedWindow="$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true)')"
-	NAME="$(echo "$focusedWindow" | grep "name" | awk '{print $2}' | grep -o '\S*"')"
-	PID="$(echo "$focusedWindow" | grep "pid" | grep -o '[0-9]*')"
+	local focused_window
+	# This is swaywm specific
+	focused_window="$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true)')"
+
+	# Get name of window and remove : ""
+	NAME="$(echo "$focused_window" | grep "name" |  grep -o ': ".*"' | sed 's/^: //;s/\"//g')"
+	# Make name lowercase
+	NAME="${NAME,,}"
+	# Get PID
+	PID="$(echo "$focused_window" | grep "pid" | grep -o '[0-9]*')"
 }
 
 function _get_config() {
@@ -18,39 +24,61 @@ function _get_config() {
 	window_name="${1//\"/}"
 
 	# Find key that best matches [title]
-	grep -m 1 -iE "^$window_name" "$config_file" | grep -Eo '".*"' | sed 's/\"//g'
+	grep -m 1 -iE "^$window_name" "$config_file" | grep -Eo '".*"'
 }
-function main() {
-	export PID NAME
+function _get_working_directory() {
 	local value
 	_get_active_window
-	value="$(_get_config "${NAME,,}")"
+
+	# Close arachne window if it is currently open 
+	if [ "$NAME" == 'arachne' ]; then 
+		swaymsg 'kill'
+		return 2
+	elif [ -z "$NAME" ]; then 
+		>&2 echo "No entry in config file matched active window: $NAME"
+		return 1
+	fi
+
+	value="$(_get_config "$NAME")"
 
 	if [ -z "$value" ]; then
-		>&2 echo "Could not find program in config file"
+		>&2 echo "No entry in config file matched active window: $NAME"
 		return 1
 	elif [ -d "$value" ]; then 
-		echo "$value"
+		WORKING_DIR="$value"
 		return 0
 	fi 
 
 	local lib_dir
 	lib_dir="$(mythos-dirs "lib" "arachne")"
 	if [ -x "$lib_dir/$value" ]; then
-		. "$lib_dir/$value" "$NAME" "$PID"
-		unset NAME PID
+		WORKING_DIR="$(. "$lib_dir/$value" "$NAME" "$PID")"
 		return $?
 	elif [ -d "$HOME/$value" ]; then
-		echo "$value"
+		WORKING_DIR="$value"
 		return 0
 	fi 
-	>&2 echo "Config value '$value' not recognized."
+	>&2 echo "No entry in config file matched active window: $NAME"
 	return 1
 }
 
+function _open_term() {
+	alacritty --config-file "$HOME/.config/mythos/arachne/arachne.yml" --working-directory "$WORKING_DIR" 
+}
+
+function main() {
+	local dir
+	_get_working_directory || return 1
+	# Move arachne-term to active workspace or start one if one DNE.
+	swaymsg '[title="Arachne"] move workspace' "$(swaymsg -t get_workspaces | jq '.. | select(.type?) | select(.focused==true) | .name')"  || _open_term")"
+	# Focus on arachne-term
+	swaymsg '[title="Arachne"] focus'
+}
 
 if [[ "$1" =~ -(h|-help)$ ]]; then
     echo "help"
 else 
+	export NAME PID WORKING_DIR
 	main "$@"
+	unset NAME PID WORKING_DIR
 fi
